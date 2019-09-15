2b252bcd96e1d5973d0a7933215303635fecda45
Danang Arbansa Nur <danang.arbansa@gmail.com>
2019-09-15 13:31:07 +0200

    Fix prettier config

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import MultiSelect from "./src";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MultiSelect from './src';
 
-const container = document.getElementById("app");
+const container = document.getElementById('app');
 
 ReactDOM.render(<MultiSelect />, container);
