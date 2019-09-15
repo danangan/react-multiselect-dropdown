0ddff612037c8ee206342f7a1cdf731735da832c
Danang Arbansa Nur <danang.arbansa@gmail.com>
2019-09-15 13:28:31 +0200

    Fix setup

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MultiSelect from "./src";
+
+const container = document.getElementById("app");
+
+ReactDOM.render(<MultiSelect />, container);
